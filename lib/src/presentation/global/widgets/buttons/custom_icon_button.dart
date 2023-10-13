8c0c2cf97f0e192b7784f81ddc325d6bbfce1d9a
Edson <edisaiassan@gmail.com>
2023-10-13 17:55:41 -0500

    Agregado CustomIconButton

---
+++ lib/src/presentation/global/widgets/buttons/custom_icon_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:species/src/presentation/global/colors.dart';
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({
+    super.key,
+    this.backgroundColor = CustomColors.whiteOpacity,
+    required this.icon,
+    this.iconColor,
+    required this.onPressed,
+    this.tooltip,
+  });
+
+  final Color? backgroundColor;
+  final String? tooltip;
+  final IconData icon;
+  final Color? iconColor;
+  final void Function()? onPressed;
+
+
+  @override
+  Widget build(BuildContext context) {
+  const double heightIcon = 40.0;
+    return Container(
+      height: heightIcon,
+      width: heightIcon,
+      decoration: BoxDecoration(color: backgroundColor, shape: BoxShape.circle),
+      child: IconButton(
+        tooltip: tooltip,
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          color: iconColor,
+        ),
+      ),
+    );
+  }
+}
