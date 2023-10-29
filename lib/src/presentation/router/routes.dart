f97589308ed4b8d67c98cb6bcf4e51c4a28546ba
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2023-10-28 19:33:42 -0500

    fix(rebase drawer): done

--- lib/src/presentation/router/routes.dart
+++ lib/src/presentation/router/routes.dart
@@ -24,4 +24,5 @@ class Routes {
   static const String specialists = '/specialists';
   static const String howToDeposit = '/how-to-deposit';
   static const String staff = '/staff';
+  static const String about = '/about';
 }
--- lib/src/presentation/router/routes_config.dart
+++ lib/src/presentation/router/routes_config.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:species/src/presentation/pages/main/left_tabs/about/about_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/indigenous_community/sub_routes/indigenous_community_details_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/species/bottom_tabs/favorites/sub_routes/image_details_favorite/image_details_favorite_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/species/bottom_tabs/favorites/sub_routes/specie_details_favorite/specie_details_favorite_page.dart';
@@ -13,16 +14,16 @@ import 'package:species/src/presentation/pages/main/left_tabs/species/bottom_tab
 import 'package:species/src/presentation/pages/main/left_tabs/species/bottom_tabs/search/search_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/species/main_species.dart';
 import 'package:species/src/presentation/pages/main/main_left_nav.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/bardcoding/bardcoding_page.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/biological_data/biological_data_page.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/how_to_deposit/how_to_deposit_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/bardcoding/bardcoding_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/biological_data/biological_data_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/how_to_deposit/how_to_deposit_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/indigenous_community/indigenous_community_page.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/maps/maps_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/maps/maps_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/profile/profile_page.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/scientific_resources/scientific_resources_page.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/specialists/specialists_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/scientific_resources/scientific_resources_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/specialists/specialists_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/staff/staff_page.dart';
-import 'package:species/src/presentation/pages/main/left_tabs/viewfinder/viewfinder_page.dart';
+// import 'package:species/src/presentation/pages/main/left_tabs/viewfinder/viewfinder_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/species/bottom_tabs/species/sub_routes/pdf_preview.dart/pdf_preview_page.dart';
 import 'package:species/src/presentation/pages/main/left_tabs/species/bottom_tabs/species/sub_routes/specie_details/specie_details_page.dart';
 import 'package:species/src/presentation/router/routes.dart';
@@ -220,75 +221,84 @@ final appRouter = GoRouter(
             ),
           ],
         ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.bardcoding,
-              name: Routes.bardcoding,
-              builder: (_, __) => const BardcodingPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.viewfinder,
-              name: Routes.viewfinder,
-              builder: (_, __) => const ViewfinderPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.maps,
-              name: Routes.maps,
-              builder: (_, __) => const MapsPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.biologicalData,
-              name: Routes.biologicalData,
-              builder: (_, __) => const BiologicalDataPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.scientificResources,
-              name: Routes.scientificResources,
-              builder: (_, __) => const ScientificResourcesPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.specialists,
-              name: Routes.specialists,
-              builder: (_, __) => const SpecialistsPage(),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: Routes.howToDeposit,
-              name: Routes.howToDeposit,
-              builder: (_, __) => const HowToDepositPage(),
-            ),
-          ],
-        ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.bardcoding,
+        //       name: Routes.bardcoding,
+        //       builder: (_, __) => const BardcodingPage(),
+        //     ),
+        //   ],
+        // ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.viewfinder,
+        //       name: Routes.viewfinder,
+        //       builder: (_, __) => const ViewfinderPage(),
+        //     ),
+        //   ],
+        // ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.maps,
+        //       name: Routes.maps,
+        //       builder: (_, __) => const MapsPage(),
+        //     ),
+        //   ],
+        // ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.biologicalData,
+        //       name: Routes.biologicalData,
+        //       builder: (_, __) => const BiologicalDataPage(),
+        //     ),
+        //   ],
+        // ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.scientificResources,
+        //       name: Routes.scientificResources,
+        //       builder: (_, __) => const ScientificResourcesPage(),
+        //     ),
+        //   ],
+        // ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.specialists,
+        //       name: Routes.specialists,
+        //       builder: (_, __) => const SpecialistsPage(),
+        //     ),
+        //   ],
+        // ),
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: Routes.howToDeposit,
+        //       name: Routes.howToDeposit,
+        //       builder: (_, __) => const HowToDepositPage(),
+        //     ),
+        //   ],
+        // ),
         StatefulShellBranch(
           routes: [
             GoRoute(
               path: Routes.staff,
               name: Routes.staff,
               builder: (_, __) => const StaffPage(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.about,
+              name: Routes.about,
+              builder: (_, __) => const AboutPage(),
             ),
           ],
         ),
