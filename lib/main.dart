5790c25d19820e32eb641278729f59fc2f0c0cfa
jmartinez888 <jhonmartinez888@gmail.com>
2023-11-04 15:19:24 -0500

    Fix ios name prop Firebase initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:species/firebase_options.dart';
 import 'package:species/src/my_app.dart';
+import 'dart:io';
 
 
 void main() async {
@@ -11,6 +13,13 @@ void main() async {
     statusBarIconBrightness: Brightness.dark,
   ));
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (Platform.isIOS) {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform, name: 'iOSApp');
+  } else if (Platform.isAndroid) {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
+  
   runApp(const ProviderScope(child: MyApp()));
 }
